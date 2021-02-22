ef0ecd24d9c59a9c77ba3ecfa49afca367b4e380
Kristyn Little <kristynlittle@frontier.com>
2021-02-22 00:16:40 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,7 @@
 def square_array(array)
-  numbers = []
-  array.each do |number ** number|
+  new_numbers = []
+  array.each do |number|
+    new_numbers = number ** number
   end
   return new_numbers
 end
